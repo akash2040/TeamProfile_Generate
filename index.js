c9eec9e7ef8b37b1e6aabb6d26a9ecdb8d9c83b5
Akash Chanara <akashchanara@gmail.com>
2022-10-25 17:09:49 -0700

    Inquirer promt

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Enginner = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const path = require("path");
+
+const questionsManager = () => {
+  inquirer.createPrompt([
+    { type: "input", name: "name", message: "What is Team Manager name?" },
+    { type: "input", name: "id", message: "What is Team Manager ID?" },
+    { type: "input", name: "email", message: "What is Team Manager Email?" },
+    {
+      type: "input",
+      name: "Officenumber",
+      message: "What is Team Manager Officenumber?",
+    },
+    { type: "list", name: "addMember", choices: ["Engineer", "Intern"] },
+  ]);
+};
